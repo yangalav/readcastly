db956170b7bd0eee41584426e525b00dafcf9b39
John Packel <john.packel@gmail.com>
2017-03-30 16:07:12 -0400

    Fixed merge conflicts again after initial rebase.

--- server/server.js
+++ server/server.js
@@ -44,3 +44,12 @@ app.listen(port, function() {
 })
 
 module.exports = app;
+// to test; will update
+app.get('/', function(req, res) {
+  console.log('server.js received GET req at / . Readcastly is on its way to fame & fortune!');
+  res.send('We heard your GET req and the diligent Readcastly hamsters are fast at work. All your wildest dreams will soon come true. Stay tuned for more exciting endpoints coming soon to a Postman near you.');
+});
+
+var port = 8999;
+app.listen(port);
+console.log('Readcastly server listening intently on port: ', port, '...yeah, baby!');
