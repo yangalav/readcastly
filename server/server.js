ec90cae04b1354c9cbe51e34df9c72ed26ea14c2
John Packel <john.packel@gmail.com>
2017-03-30 18:56:22 -0400

    dealt with merge conflict 3

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var urlParser = bodyParser.urlencoded({extended: false});
+
+app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
@@ -12,6 +16,25 @@ app.get('/', function(req, res) {
   console.log('server.js received GET req at / . Readcastly is on its way to fame & fortune!');
   res.send('We heard your GET req and the diligent Readcastly hamsters are fast at work. All your wildest dreams will soon come true. Stay tuned for more exciting endpoints coming soon to a Postman near you.');
 });
+
+// to test urlParser; will update to add authentication route when we get to that story
+app.post('/login', urlParser, function(req, res) {
+  if(!req.body) {
+    console.log('server.js . 17 - urlParser says no body on this request: ', req);
+    return res.sendStatus(400);
+  }
+  res.send('Welcome to Readcastly, ', req.body.username, '! Nice to have you on board.');
+})
+
+// to test bodyParser for json;
+app.post('/jsonTest', bodyParser, function(req, res) {
+  if(!req.body) {
+    console.log('server.js l. 26: jsonTest did not get an object to parse');
+    return res.sendStatus(400);
+  }
+  console.log('server.js l. 29: req.body should be an obj. body = ', req.body);
+  res.sendStatus(200);
+})
 
 var port = 8999;
 app.listen(port);
