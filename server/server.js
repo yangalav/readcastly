d68d11a7d2349484df6e419a0baa220afd40f832
John Packel <john.packel@gmail.com>
2017-03-30 18:54:34 -0400

    dealt with merge conflict 2

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
 
-// to test; will update with the actual endpoint in next user story
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on port", port);
+})
+
+module.exports = app;
+// to test; will update
 app.get('/', function(req, res) {
   console.log('server.js received GET req at / . Readcastly is on its way to fame & fortune!');
   res.send('We heard your GET req and the diligent Readcastly hamsters are fast at work. All your wildest dreams will soon come true. Stay tuned for more exciting endpoints coming soon to a Postman near you.');
