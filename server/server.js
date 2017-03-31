c44f4a363e1d75bf09d6a12819973d12b2975045
John Packel <john.packel@gmail.com>
2017-03-31 00:25:17 -0400

    Added npm request & /requrl/:requrl. Only getting data back from Mercury API without 'http'; need to debug.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var urlParser = bodyParser.urlencoded({extended: false});
 var jsonParser = bodyParser.json();
+var request = require('request');
+// var saveTextToDB = require('./controllers/dbController');
 
 app.use(bodyParser.json());
 
@@ -12,9 +14,31 @@ app.use(express.static(path.join(__dirname, './client')));
 app.post('/requrl/:requrl', function(req, res) {
   let requrl = req.params.requrl;
   console.log('server.js POST to requrl. l. 14. req.params.url = ', req.params.requrl);
-  res.status(200).send('Got your request to listen to the text of ' + req.params.requrl);
+  var objToSaveToDB = {
+    requrl: 'requrl'
+  };
 
-// res.status(200).send('Got your request to listen to the text ');
+  request({
+    'method': 'GET',
+    'x-api-key': 'KmjXDnLR5Dmtn2IPHQCwONFAFUlaJQpObfJq0AM6',
+    'uri': 'https://mercury.postlight.com/parser?url=' + req.params.requrl,
+    'Content-Type': 'application/json'
+  }, function(error, response, body) {
+    console.log('server.js GET req to Mercury, l. 22. body received = ', body);
+    if(error) {
+      console.log('server.js, GET req to Mercury. error! = ', console.error);
+      res.status(400).send('Dang; error retrieving parsed text of url from Mercury...');
+    }
+    // else {
+    //   objToSaveToDB.
+    //   // saveTextToDB.saveToDB(body);
+    //
+    // }
+  }
+)
+
+
+  // res.status(200).send('Got your request to listen to the text of ' + req.params.requrl);
 });
 
 // to test; will update with the actual endpoint in next user story
@@ -47,10 +71,28 @@ app.post('/jsonTest', jsonParser, function(req, res) {
   res.sendStatus(200);
 })
 
-
 var port = process.env.PORT || 8080;
+
 app.listen(port, function() {
   console.log("Readcastly server listening intently on port:", port);
 })
 
 module.exports = app;
+
+// sample response from Mercury:
+// {
+//   "title": "Building Awesome CMS",
+//   "content": "<div><div><div class=\"section-content\"><div.... more content",
+//   "author": "Jeremy Mack",
+//   "date_published": "2016-10-03T12:48:58.385Z",
+//   "lead_image_url": "https://cdn-images-1.medium.com/max/1200/1*zo51eqdjJ_XSU0D8Vm8P9A.png",
+//   "dek": null,
+//   "next_page_url": null,
+//   "url": "https://trackchanges.postlight.com/building-awesome-cms-f034344d8ed",
+//   "domain": "trackchanges.postlight.com",
+//   "excerpt": "Awesome CMS is…an awesome list of awesome CMSes. It’s on GitHub, so anyone can add to it via a pull request.",
+//   "word_count": 397,
+//   "direction": "ltr",
+//   "total_pages": 1,
+//   "rendered_pages": 1
+// }
