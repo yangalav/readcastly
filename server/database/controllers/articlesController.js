9049bcfe7af2b9fc82b5107dda1a89dc01bed169
Andrew Fechner <arfechner@gmail.com>
2017-04-01 11:26:45 -0400

    chore(database/server): Complete function to add article to database and user library

--- server/database/controllers/articlesController.js
+++ server/database/controllers/articlesController.js
@@ -2,28 +2,65 @@ const db = require('../dbConfig');
 const Promise = require('bluebird');
 const Articles = require('../collections/articles');
 const Article = require('../models/article');
+const ArticlesUsers = require('../collections/articles-users');
+const ArticleUser = require('../collections/article-user');
 const SourceCon = require('./sourcesController')
 
 var getSource = Promise.promisify(SourceCon.getSource);
 
 exports.create = function(articleData) {
-  getSource(articleData.domain)
-    .then(function(source) {
-      Articles.create({
-        url: articleData.url,
-        title: articleData.title,
-        author: articleData.author,
-        publication_date: articleData.publication_date,
-        source_id: SourceCon.sourceId,
-        text: articleData.text,
-        image: articleData.image,
-        excerpt: articleData.excerpt,
-        word_count: articleData.word_count,
-        est_time: articleData.est_time,
-        created_by: articleData.created_by
+  new Article({url: articleData.url}).fetch()
+    .then(function(found) {
+      if (found) {
+        new ArticleUser({article_id: found.id,user_id: articleData.user_id}).fetch()
+          .then(function(alsoFound) {
+            if (alsoFound) {
+              console.log("ARTICLE ALREADY IN USER'S LIBRARY - NEED TO FIGURE OUT HOW TO PASS ERROR UP TO FRONT END");
+            } else {
+              ArticlesUsers.create({
+                article_id: found.id,
+                user_id: articleData.user_id
+              })
+              .then(function(articleUserEntry) {
+                console.log('EXISTING ARTICLE FOUND, NEW ENTRY CREATED', articleUserEntry);
+              })
+              .catch(function(error) {
+                console.log('ERROR CREATING NEW ENTRY FOR EXISTING ARTICLE', error);
+              })
+            }
+          })
+          .catch(function(error) {
+            console.log("ERROR CHECKING IF ARTICLE ALREADY IN USER'S LIBRARY", error)
+          })
+      } else {
+        getSource(articleData.domain)
+          .then(function(source) {
+            Articles.create({
+              url: articleData.url,
+              title: articleData.title,
+              author: articleData.author,
+              publication_date: articleData.publication_date,
+              source_id: SourceCon.sourceId,
+              text: articleData.text,
+              image: articleData.image,
+              excerpt: articleData.excerpt,
+              word_count: articleData.word_count,
+              est_time: articleData.est_time,
+              created_by: articleData.user_id
+            })
+          })
+          .then(function(article) {
+            ArticlesUsers.create({
+              article_id: article.id,
+              user_id: article.created_by
+            })
+          })
+          .catch(function(error) {
+            console.log('ERROR CREATING NEW ARTICLE', error);
+          })
+      }
     })
-    .then(function(article) {
-      //ADD TO JOIN TABLE
-  }
-
+    .catch(function(error) {
+      console.log('ERROR CHECKING FOR EXISTING ARTICLE', error);
+    })
 }
