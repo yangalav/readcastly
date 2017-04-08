e3882359d435f323926cb9535d34e1fd78d19595
Viswada Yangala <yangalav@go.stockton.edu>
2017-04-08 11:51:04 -0400

    avoiding merge conflict

--- server/routes.js
+++ server/routes.js
@@ -82,10 +82,19 @@ module.exports = function(app, express) {
     });
   });
 
+  app.post('/texter', function(req,res) {
+    let readcast = utils.readcastBuilder(req.body);
+    //req.body will need all fields required for conversion, including title, author, and source, at a minimum, in addition to text
+    //invoke function that converts article to speech, grab path - AUDIO FORMAT RETURNED MUST BE MP4, MPEG, OR OGG
+    readcast.location = //path to file;
+    texter.sendText(User.currentPhone,readcast,function(confirmation){
+      res.send(confirmation);
+    });
+  });
+
   app.get('/signup', function(req,res) {
       res.send('this is our signup page :)');
   });
-
 
 };
 
