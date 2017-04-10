e009772fda845ed538fd0ab81fc1cd88c9f57f54
Viswada Yangala <yangalav@go.stockton.edu>
2017-04-10 11:41:49 -0400

    editing middleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 const urlParser = bodyParser.urlencoded({extended: false});
 const jsonParser = bodyParser.json();
 const request = require('request');
@@ -14,6 +17,27 @@ const news = require('./apis/newsController');
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(session({
+  store: new
+  secret: 'sassy llama',
+  resave: false,
+  saveUninitialized: true
+}));
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 var port = process.env.PORT || 8080;
 
