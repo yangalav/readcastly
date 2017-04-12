f2624bbc33f0248cb4d50522ed58bb904e698557
Andrew Fechner <arfechner@gmail.com>
2017-04-12 19:50:23 -0400

    Abandon checkboxes for now; Randomizer in place

--- client/components/Checkboxes.jsx
+++ client/components/Checkboxes.jsx
@@ -1,26 +1,6 @@
 import React from 'react';
 import {Checkbox, Row, Col, FormGroup, FormControl, Button} from 'react-bootstrap';
 
-const sources = [
-  {id: "associated-press", name: 'Associated Press'},
-  {id: 'bbc-news', name: 'BBC News'},
-  {id: 'business-insider', name: 'Business Insider'},
-  {id: 'cnn', name: 'CNN'},
-  {id: 'daily-mail', name: 'Daily Mail'},
-  {id: 'the-economist', name: 'The Economist'},
-  {id: 'entertainment-weekly', name: 'Entertainment Weekly'},
-  {id: 'espn', name: 'ESPN'},
-  {id: 'hacker-news', name: 'Hacker News'},
-  {id: 'new-scientist', name: "New Scientist"},
-  {id: 'new-york-magazine', name: 'New York Magazine'},
-  {id: 'the-new-york-times', name: 'The New York Times'},
-  {id: 'recode', name: 'Recode'},
-  {id: 'techcrunch', name: 'TechCrunch'},
-  {id: 'time', name: 'Time'},
-  {id: 'the-washington-post', name: 'The Washington Post'},
-  {id: 'usa-today', name: 'Time'}
-];
-
 let choices = [];
 
 class Checkboxes extends React.Component {
@@ -53,7 +33,7 @@ class Checkboxes extends React.Component {
   }
 
   createCheckboxes() {
-    sources.map(this.createCheckbox);
+    this.props.sources.map(this.createCheckbox);
   }
 
   render() {
@@ -62,10 +42,10 @@ class Checkboxes extends React.Component {
         <Row>
           <Col md={12}>
             <FormGroup onSubmit={this.handleFormSubmit}>
-                {sources.map((source) => (
+                {this.props.sources.map((source) => (
                       <div className="checkbox" key={source.id}>
                           <label>
-                          <input type="checkbox" value={source.id} onChange={this.toggleCheckbox}/>
+                          <input type="checkbox" value={source.id} onChange={this.toggleCheckbox()}/>
                           <span className="cr"><i className="cr-icon glyphicon glyphicon-ok"></i></span>
                             {source.name}
                           </label>
--- client/components/TopStories.jsx
+++ client/components/TopStories.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
 import Checkboxes from './Checkboxes.jsx'
 import ArticleList from './ArticleList.jsx'
-// import {Button, Col, FormControl, FormGroup, Checkbox} from 'react-bootstrap';
+import {Grid, Button, Col, Row} from 'react-bootstrap';
+
+const sources = [
+  {id: "associated-press", name: 'Associated Press'},
+  {id: 'bbc-news', name: 'BBC News'},
+  {id: 'business-insider', name: 'Business Insider'},
+  {id: 'cnn', name: 'CNN'},
+  {id: 'daily-mail', name: 'Daily Mail'},
+  {id: 'the-economist', name: 'The Economist'},
+  {id: 'entertainment-weekly', name: 'Entertainment Weekly'},
+  {id: 'espn', name: 'ESPN'},
+  {id: 'hacker-news', name: 'Hacker News'},
+  {id: 'new-scientist', name: "New Scientist"},
+  {id: 'new-york-magazine', name: 'New York Magazine'},
+  {id: 'the-new-york-times', name: 'The New York Times'},
+  {id: 'recode', name: 'Recode'},
+  {id: 'techcrunch', name: 'TechCrunch'},
+  {id: 'time', name: 'Time'},
+  {id: 'the-washington-post', name: 'The Washington Post'},
+  {id: 'usa-today', name: 'Time'}
+];
 
 class TopStories extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { randomMode : true };
   }
+
+  randomizer(){
+    let randomSource = sources[Math.floor(Math.random()*sources.length)];
+    // this.props.getTopStories([randomSource.id]);
+    console.log('YOUR RANDOM SOURCE WOULD HAVE BEEN: ', randomSource.name);
+  }
+
+  // componentDidMount() {
+  //   this.randomizer();
+  // }
 
   render() {
     return (
       <div className="topStories">
-        <Checkboxes getTopStories={this.props.getTopStories.bind(this)} />
+        {/*<Checkboxes sources={sources} getTopStories={this.props.getTopStories.bind(this)} />*/}
+        <Grid>
+          <Row>
+            <Col md={4} md-offset={4}>
+              <Button bsStyle="success" bsSize="large" onClick={this.randomizer} block>Get headlines from a random source</Button>
+            </Col>
+          </Row>
+        </Grid>
         {this.props.headlines && <ArticleList articles={this.props.headlines} user={this.props.user} deleteIt={this.props.deleteIt.bind(this)} convertIt={this.props.convertIt.bind(this)} exportOptions={this.props.exportOptions} topStoryMode={this.props.topStoryMode}/>}
       </div>
     );
