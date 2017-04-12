d5f69a9578fed55a6a92c5659371d1c4a73c6d49
Viswada Yangala <yangalav@go.stockton.edu>
2017-04-12 14:57:54 -0400

    added form fields for first/last name, phone, voicepref, avatar

--- client/components/SignupForm.jsx
+++ client/components/SignupForm.jsx
@@ -6,29 +6,70 @@ class SignupForm extends React.Component {
   constructor() {
 		super();
 
-    this.state = { email: '', password: '' };
+    this.state = {
+      firstName: '',
+      email: '',
+      password: '',
+      lastName: '',
+      phone: '',
+      voicePref: '',
+      avatar: '',
+    };
 
-    this.handleSignUp = this.handleSignUp.bind(this);
+    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+    this.handleLastNameChange = this.handleLastNameChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
-	}
+    this.handlePhoneChange = this.handlePhoneChange.bind(this);
+    this.handleVoicePrefChange = this.handleVoicePrefChange.bind(this);
+    this.handleAvatarChange = this.handleAvatarChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+  }
 
-  handleSignUp(e) {
+  handleSignUp() {
     console.log("The email is: " + this.state.email);
     console.log("The password is: " + this.state.password);
-    axios.post('/signup', {email: this.state.email, password: this.state.password})
+    axios.post('/signup', {
+      email: this.state.email,
+      password: this.state.password,
+      first_name: this.state.firstName,
+      last_name: this.state.lastName,
+      phone: this.state.phone,
+      voice_pref: this.state.voicePref,
+      avatar: this.state.avatar,
+    })
 		.then((res) => {
 			console.log("POST REQUESTTTTTTTTTTTTTTTTTTTTTT");
 			return;
 		})
   }
 
+  handleFirstNameChange(e) {
+    this.setState({ firstName: e.target.value });
+  }
+
+  handleLastNameChange(e) {
+    this.setState({ lastName: e.target.value });
+  }
+
   handleEmailChange(e) {
-    this.setState({email: e.target.value});
+    this.setState({ email: e.target.value });
   }
 
   handlePasswordChange(e) {
-    this.setState({password: e.target.value});
+    this.setState({ password: e.target.value });
+  }
+
+  handlePhoneChange(e) {
+    this.setState({ phone: e.target.value });
+  }
+
+  handleVoicePrefChange(e) {
+    this.setState({ voicePref: e.target.value });
+  }
+
+  handleAvatarChange(e) {
+    this.setState({ avatar: e.target.value });
   }
 
   render() {
@@ -36,16 +77,20 @@ class SignupForm extends React.Component {
       <div>
         <form>
           First Name:<br/>
-          <input type="text" name="firstName"/><br/>
+        <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleFirstNameChange}/><br/>
           Last Name:<br/>
-          <input type="text" name="lastName"/><br/>
+        <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleLastNameChange}/><br/>
           E-mail:<br/>
-          <input type="email" name="email" value={this.state.email} onChange={this.handleEmailChange}/><br/>
+        <input type="email" name="email" value={this.state.email} onChange={this.handleEmailChange}/><br/>
           Password:<br/>
         <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/><br/>
+          Phone Number:<br/>
+        <input type="text" name="phone" value={this.state.phone} onChange={this.handlePhoneChange}/><br/>
+          Voice Preference:<br/>
+        <input type="text" name="voicePref" value={this.state.voicePref} onChange={this.handleVoicePrefChange}/><br/>
           Avatar:<br/>
-          <input type="url" name="avatar"/><br/>
-          <Button bsStyle="success" onClick={this.handleSignUp}>Sign Up</Button>
+        <input type="url" name="avatar" value={this.state.avatar} onChange={this.handleAvatarChange}/><br/>
+        <Button bsStyle="success" onClick={this.handleSignUp}>Sign Up</Button>
         </form>
       </div>
   	);
