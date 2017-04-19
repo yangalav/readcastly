84a3d727479be287da87951bf38b5a202146e0e8
John Packel <john.packel@gmail.com>
2017-04-19 15:51:59 -0400

    Updated player to play welcome tracks (2 default URLs) until user selects a readcast. Wrote welcome text and will add URLs once processed.

--- client/components/player.jsx
+++ client/components/player.jsx
@@ -4,8 +4,9 @@ import AudioPlayer from 'react-responsive-audio-player';
 const Player = ({track}) => (
   <div className="audio-player">
   {
-    (!track)
-    ? "Please wait ..."
+    (track.url === null)
+    ? <AudioPlayer playlist={[{url: 'https://s3.amazonaws.com/readcastly-user-files/king-now-is-the-perfect-1303-joanna.mp3', displayText: 'KING: Now is the perfect time to discuss how and why Bernie Sanders could\'ve beaten Donald Trump'}, {url: 'https://s3.amazonaws.com/readcastly-user-files/the-white-houses-misleading-statements-1304-salli.mp3', displayText: 'The White House’s misleading statements about Trump’s ‘armada’ heading to North Korea'}]} hideBackSkip={true} style={{ position: 'fixed', bottom: 0 }} />
+
     : <AudioPlayer playlist={[{url: track.url, displayText: track.title}]} hideBackSkip={true} style={{ position: 'fixed', bottom: 0 }} />
   }
   </div>
