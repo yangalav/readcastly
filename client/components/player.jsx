df6d3aefa9c3bf55fb76ac125b55186b86b6c973
John Packel <john.packel@gmail.com>
2017-04-20 16:34:29 -0400

    fix merge conflicts

--- client/components/player.jsx
+++ client/components/player.jsx
@@ -7,7 +7,7 @@ const Player = ({track}) => (
     (track.url === null)
     ? <AudioPlayer autoplay cycle={false} playlist={[{url: 'https://s3.amazonaws.com/readcastly-user-files/readcastly-welcome-part01.mp3', displayText: 'Welcome to ReadCast.ly!'}, {url: 'https://s3.amazonaws.com/readcastly-user-files/readcastly-welcome-part02.mp3', displayText: 'Welcome to ReadCast.ly (continued)'}]} hideBackSkip={true} style={{ position: 'fixed', bottom: 0 }} />
 
-    : <AudioPlayer playlist={[{url: track.url, displayText: track.title}]} hideBackSkip={true} cycle={false} style={{ position: 'fixed', bottom: 0 }} />
+    : <AudioPlayer autoplay cycle={false} playlist={[{url: track.url, displayText: track.title}]} hideBackSkip={true} style={{ position: 'fixed', bottom: 0 }} />
   }
   </div>
 );
