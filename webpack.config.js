bb3667dfc0416ba9bba2edde619f3d17617919ba
Andrew Fechner <arfechner@gmail.com>
2017-04-21 13:51:03 -0400

    John M webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,19 @@ const config = {
       },
     ],
   },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
   watch: true
 };
 
